a5aea3f439895a09147b4bfe1cdba5a1900befba
Wernerzucho <wernerzucho@gmail.com>
2018-01-08 16:21:06 -0600

    Bitwise with menu

--- Code3.py
+++ Code3.py
@@ -1,16 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------#
+__author__ = "Werner Suchowitzki Orozco"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Werner Suchowitzki Orozco"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Werner Suchowitzki Orozco"
+__email__ = "wsuchowitzki@stengineeringsolutions.com"
+__status__ = "Init"
 
-#Strings and print type
-x = "Hola mundo" #Is the same
-z = 'Adios mundo' #Is the same
-print(x)
-print(z)
-y = type(x)
-print(y)
+#------------------------------------------------------------------------------#
+#LIBRARIES:
+import random
+import binascii
+#------------------------------------------------------------------------------#
 
-#Exponentiation
-num = 4**2
-print(num)
+#------------------------------------------------------------------------------#
+#FUNCTIONS:
+def menu():
+    option = input("""
+    Code3 Bitwise:
+    1) AND
+    2) OR
+    3) XOR
+    4) Shift
+    Option: """)
 
-#Boolean
-proof = False #For boolean is capital F or capital T
-print(proof)
+    return(option)
+
+
+def bitwise(case):
+    list = [0x0f,0x10,0xa0,0xaa,0x01,0xba,0x00,0x52,0xcc,0xca,\
+            0x0f,0x10,0xa0,0xaa,0x01,0xba,0x00,0x52,0xcc,0xca]
+
+    list2 = []
+
+    mask = 0x01
+
+    print("List:")
+    print('[{}]'.format(', '.join(hex(x) for x in list)))
+
+    if case == '1':
+        print(case)
+
+        #AND
+        for pos in list:
+            list2.append(pos & mask)
+
+        print("AND:")
+
+    elif case == '2':
+        #OR
+        for pos in list:
+            list2.append(pos | mask)
+        
+        print("OR:")
+
+    elif case == '3':
+        #XOR
+        for pos in list:
+            list2.append(pos ^ mask)
+        
+        print("XOR:")
+
+    elif case == '4':
+        #Shift
+        shift = int(input("Shift number: "))
+        for pos in list:
+            list2.append(pos << shift)
+        
+        print("Shift:")
+
+    else:
+        print("Incorrect option")
+
+    print('[{}]'.format(', '.join(hex(x) for x in list2)))
+
+#------------------------------------------------------------------------------#
+#MAIN
+if __name__ == "__main__":
+    switch = menu()
+    bitwise(switch)
+#------------------------------------------------------------------------------#
