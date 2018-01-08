a7b745f9fefd54fd93a42be875f3e6aca3b1127d
Wernerzucho <wernerzucho@gmail.com>
2018-01-08 12:28:52 -0600

    Clases y objetos

--- Code2.py
+++ Code2.py
@@ -1,16 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------
+__author__ = "Werner Suchowitzki Orozco"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Werner Suchowitzki Orozco"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Werner Suchowitzki Orozco"
+__email__ = "wsuchowitzki@stengineeringsolutions.com"
+__status__ = "Init"
 
-#Strings and print type
-x = "Hola mundo" #Is the same
-z = 'Adios mundo' #Is the same
-print(x)
-print(z)
-y = type(x)
-print(y)
+#--------------------------------------------------------
+#LIBRARIES:
+import random
+#-------------------------------------------
+# -------------
+#FUNCTIONS:
+class MyFirstClass():
 
-#Exponentiation
-num = 4**2
-print(num)
+    def __init__(self):
+        self.list1 = [1,2,3,4,5,6,7,8,9,10]
+        self.list2 = []
+        self.a = 5
+        self.b = random.randint(1,10)
 
-#Boolean
-proof = False #For boolean is capital F or capital T
-print(proof)
+    def funct1(self):
+        if(self.a == self.b):
+            print(self.b)
+            print("I´m winner")
+        else:
+            print(self.b)
+            print("I´m loser")
+
+
+    def funct2(self):
+        for i in self.list1:
+            self.list2.append(i)
+            print(i)
+        
+        print(len(self.list1))
+        print(len(self.list2))
+
+        random.shuffle(self.list2)
+        if(self.list2[5] == 6):
+            print("Correct")
+        else:
+            print("Fail")
+
+
+
+#-------------------------------------------------------
+#MAIN
+if __name__ == "__main__":
+    obj = MyFirstClass()
+    obj.funct1()
+    obj.funct2()
+#-----------------------------------------------------
